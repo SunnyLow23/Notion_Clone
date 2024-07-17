1e4047c1cc9319aa8b1b2af873024ebb75d60703
lqnquang <21020381@vnu.edu.vn>
2024-07-17 09:49:59 +0700

    Initial commit

---
+++ backend/src/main/java/com/sunnylow/notion_clone/NotionCloneApplication.java
@@ -0,0 +1,13 @@
+package com.sunnylow.notion_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sunnylow/notion_clone/NotionCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnylow.notion_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
